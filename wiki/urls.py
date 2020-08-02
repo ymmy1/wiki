968d53a760179ecd769e407f23bd5a264d471b1c
ymmy1 <chem-pion@mail.ru>
2020-08-01 23:32:49 -0700

    second commit

--- wiki/urls.py
+++ wiki/urls.py
@@ -18,5 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("encyclopedia.urls")),
+    path('', include("encyclopedia.urls")),
     path('', include("encyclopedia.urls"))
 ]
