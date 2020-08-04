2a6df93f3db6ec9b817cd73b54abc4c652db5527
ymmy1 <chem-pion@mail.ru>
2020-08-04 02:21:24 -0700

    Random Page & Edit Page done

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,6 +6,8 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:name>", views.wiki, name="wiki"),
+    path("wiki/<str:name>/edit", views.edit, name="edit"),
     path("search", views.search, name="search"),
-    path("create", views.create, name="create")
+    path("create", views.create, name="create"),
+    path("random", views.random, name="random")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django import forms
 from django.http import HttpResponseRedirect
+import secrets
 from . import util
 
 class NewSearchForm(forms.Form):
     q = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'search', 'placeholder':"Search Encyclopedia" }))
+
 class NewCreateForm(forms.Form):
     title = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'create_title', 'placeholder':"Title" }))
-    content = forms.CharField(label="", widget=forms.Textarea(attrs={'class':'create', 'placeholder':"Content" }))
-     
+    content = forms.CharField(label="", widget=forms.Textarea(attrs={'class':'create_content', 'placeholder':"Content" }))
+
+class NewEditForm(forms.Form):
+    content = forms.CharField(label="", widget=forms.Textarea(attrs={'class':'edit_content'}) )
+
+
 
 
 def index(request):
@@ -17,28 +23,53 @@ def index(request):
         "form": NewSearchForm()
     })
 
+def random(request):
+    title = secrets.choice(util.list_entries())
+    return HttpResponseRedirect(f'/wiki/{title}')
+
+
 def create(request):
-    print("request method is " + request.method)
     if request.method == "POST":
-        print("form is POST")
         form = NewCreateForm(request.POST)
         if form.is_valid():
-            print("form is Valid")
             title = form.cleaned_data["title"]
             content = form.cleaned_data["content"]
             util.save_entry(title, content)
-            print("ENTRY SAVED")
             return HttpResponseRedirect(f'/wiki/{title}')
     return render(request, "encyclopedia/create.html", {
             "form": NewSearchForm(),
             "form_create": NewCreateForm()
         })
 
+def edit(request, name):
+    
+    if request.method == "POST":
+        print("START POST")
+        form = NewEditForm(request.POST)
+        if form.is_valid():
+            print("START VALID")
+            title = name
+            content = form.cleaned_data["content"]
+            util.save_entry(title, content)
+            print("START SAVED")
+            return HttpResponseRedirect(f'/wiki/{name}')
+    
+    else:
+        page = util.get_entry(name)
+        
+        context = {
+            'form_edit': NewEditForm(initial={'content': page}),
+            'title': name
+        }
+
+        return render(request, "encyclopedia/edit.html", context)
+
 
 def wiki(request, name):
-     return render(request, "encyclopedia/item.html", {
+    return render(request, "encyclopedia/item.html", {
         "entry": util.get_entry(name),
-        "form": NewSearchForm()
+        "form": NewSearchForm(),
+        "title": name
     })
 
 def search(request):
